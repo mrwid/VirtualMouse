ed48c0cd2142593f84a8b355bbafac5844ca7f7a
Mr.Wid <mr_wid@163.com>
2013-03-24 02:08:46 +0800

    自定义的标识符常量 ==================
    wid

---
+++ constant.h
@@ -0,0 +1,35 @@
+#pragma once
+
+//////////////////////////////////////////////////////////////////////////
+
+#define		ID_HOT_UP			10000			//上
+#define		ID_HOT_DOWN			10001			//下
+#define		ID_HOT_LEFT			10002			//左
+#define		ID_HOT_RIGHT		10003			//右
+#define		ID_HOT_TOPLEFT		10004			//左上
+#define		ID_HOT_TOPRIGHT		10005			//右上
+#define		ID_HOT_BOTLEFT		10006			//左下
+#define		ID_HOT_BOTRIGHT		10007			//右下
+
+#define		ID_HOT_SL_UP		10008			//微调, 上
+#define		ID_HOT_SL_DOWN		10009			//微调, 下
+#define		ID_HOT_SL_LEFT		10010			//微调, 左
+#define		ID_HOT_SL_RIGHT		10011			//微调, 右
+
+#define		ID_HOT_LEFT_CLICK	10100			//左击
+#define		ID_HOT_RIGHT_CLICK	10101			//右击
+
+#define		ID_HOT_MIDDLE_UP	10200			//滚轮上
+#define		ID_HOT_MIDDLE_DOWN	10201			//滚轮下
+
+#define		ID_HOT_MIDDLE_PRESS	10300			//中键按下
+
+//////////////////////////////////////////////////////////////////////////
+
+#define		ID_BTN_PAUSE		10400			//暂停\继续模拟
+#define		ID_BTN_HIDE			10401			//隐藏模拟
+#define		ID_BTN_EXIT			10402			//退出模拟
+
+//////////////////////////////////////////////////////////////////////////
+
+#define		ID_HOT_WND_HIDE		10500			//隐藏\显示窗口
